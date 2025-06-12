f1c003b431dbb1538736f8db1d2d018cef877975
Jidnyesh Chaudhari <jidnyesh@Jidnyeshs-MacBook-Air.local>
2025-06-13 02:05:14 +0530

    My Live Search Weather application

---
+++ build/precache-manifest.6133e5f89df51f335eeed06bf0d7e471.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bcf518560893351e7ed43ffd616ec0",
+    "url": "/Live-Search-Weather-Application/index.html"
+  },
+  {
+    "revision": "fa235f923fdb46f1a44b",
+    "url": "/Live-Search-Weather-Application/static/css/main.0da854cd.chunk.css"
+  },
+  {
+    "revision": "6dcfc77f0f18ba424a25",
+    "url": "/Live-Search-Weather-Application/static/js/2.701fe897.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Live-Search-Weather-Application/static/js/2.701fe897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa235f923fdb46f1a44b",
+    "url": "/Live-Search-Weather-Application/static/js/main.3230abf8.chunk.js"
+  },
+  {
+    "revision": "87a3f974554db5b053fd",
+    "url": "/Live-Search-Weather-Application/static/js/runtime-main.2fe3e5bc.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Live-Search-Weather-Application/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Live-Search-Weather-Application/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Live-Search-Weather-Application/static/media/city.e1698332.jpg"
+  }
+]);
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "3bd3ee9e190f92bac2cf85a47a9a82ae",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
